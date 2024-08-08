c7a60b4370b94f26b7c521c0e0631b9b23bfff54
TeodorBirghisan <birghisanteodor@gmail.com>
2024-08-08 17:27:11 +0300

    update api config url

--- TodoUi/src/constants/api.config.ts
+++ TodoUi/src/constants/api.config.ts
@@ -1,11 +1,9 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL;
-
 const API_ENDPOINTS = {
   TODOS: {
-    GET_ALL: `${API_BASE_URL}/api/TodoItems`,
-    CREATE: `${API_BASE_URL}/api/TodoItems`,
-    UPDATE: (id: number) => `${API_BASE_URL}/api/TodoItems/${id}`,
-    DELETE: (id: number) => `${API_BASE_URL}/api/TodoItems/${id}`,
+    GET_ALL: `/api/TodoItems`,
+    CREATE: `/api/TodoItems`,
+    UPDATE: (id: number) => `/api/TodoItems/${id}`,
+    DELETE: (id: number) => `/api/TodoItems/${id}`,
   },
 };
 
